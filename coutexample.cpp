b495aa2150a97dabe5e30fcefc5ff0c70bfcac5a
lscraig42 <lscraig42@students.tntech.edu>
2016-09-01 21:24:49 -0500

    Add files via upload

---
+++ coutexample.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "whatsup?";
+	cout << endl;
+	cout << "\n";
+	cout << "Hi";
+	cout << "on";
+	cout << "different";
+	cout << "lines";
+	cout << "Hi\non\ndifferent\nlines\nfor\nreal\n";
+	cout << "Another " << "example" << endl;
+	return 0;
+}
