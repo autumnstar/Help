1d08db2ceb6e746bb3a1754eac12154760360ca0
autumnstar <adouglas42@students.tntech.edu>
2016-09-01 21:24:45 -0500

    Add files via upload

---
+++ coinFlip.cpp
@@ -0,0 +1,48 @@
+/*
+	Dr. David Brown
+	18 Feb 16
+	CSC2100
+	This is a perfect example of what Program 1 should be - a coin flip guessing game
+	that allows the user to guess for only one coin flip
+*/
+
+#include <iostream>
+#include "Random.h"
+
+using namespace std;
+
+int main()
+{
+	int userGuess,			//1 or 0 indicating user guess of heads or tails
+		coinFlip;			//Randomly generated 0 or 1 indicating coin flip result
+	string headsTails;		//Heads or Tails string giving user output
+		
+	seedRandom();			//Seed the randomizer to give good random numbers
+	
+	//Get the users guess to the coin flip
+	cout << "Welcome to the Coin Flip Toss" << endl;
+	cout << "Please enter your choice (0 for heads, 1 for tails) : ";
+	cin >> userGuess;
+	
+	//Did the user guess heads or tails
+	if (userGuess == 0)
+		headsTails = "heads";
+	else
+		headsTails = "tails";
+	
+	//Generate the result for the coin flip
+	coinFlip = getRandomInt (0, 2);
+	
+	//See if the user got it right
+	if (userGuess == coinFlip)
+	{
+		cout << "You chose " << headsTails << ", congratulations you guessed correctly." << endl;
+		cout << "You win!" << endl;
+	}
+	else		//They lost so let them know...
+	{
+		cout << "You chose " << headsTails << ", sorry you have guessed incorrectly." << endl;
+		cout << "The Computer wins" << endl;
+	}
+	
+}
